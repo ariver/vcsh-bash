4c156d7934a7e51b718232f840432b973174fbac
Aaron River <aaron.river@rackspace.com>
2014-09-06 04:50:57 -0500

    make some vars readonly

--- .config/bash/_profile.d/30_export.d/01_xdg.bash
+++ .config/bash/_profile.d/30_export.d/01_xdg.bash
@@ -5,11 +5,13 @@
 #
 # http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 #
-set -a
+
 : ${XDG_CONFIG_HOME:=${HOME}/.config}
 : ${XDG_CONFIG_DIRS:=/etc/xdg}
 : ${XDG_DATA_HOME:=${HOME}/.local/share}
 : ${XDG_DATA_DIRS:=/usr/local/share/:/usr/share/}
 : ${XDG_CACHE_HOME:=${HOME}/.cache}
 : ${XDG_RUNTIME_DIR:=${TMPDIR:-/tmp}}
-set +a
+
+export   ${!XDG_*}
+readonly ${!XDG_*}
--- .config/bash/_profile.d/30_export.d/05_delimiters.bash
+++ .config/bash/_profile.d/30_export.d/05_delimiters.bash
@@ -1,18 +1,17 @@
 #! /dev/null/bash
 
-set -a
-
 #
 ## Delimeters
 #
 
 # Tab-character
-printf -v TC_TAB "\t"
+printf -v TC_TAB '\t'
 
 # Newline-character
-printf -v TC_NLN "\n"
+printf -v TC_NLN '\n'
 
 # Return-character
-printf -v TC_CRT "\r"
+printf -v TC_CRT '\r'
 
-set +a
+export   ${!TC_*}
+readonly ${!TC_*}
