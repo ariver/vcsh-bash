26ff801f544b2ab1ccbe6948ee92d6c916d583a5
Aaron River <aaron.river@rackspace.com>
2014-09-14 12:32:21 -0500

    rolled in a tmp fnc that loads simple completions.

--- .config/bash/rc.d/92_completion.bash
+++ .config/bash/rc.d/92_completion.bash
@@ -6,3 +6,38 @@
 eval "$( pip completion --bash )"
 
 #eval "$( printf 'function ___test1 () { printf "%s\n" "${FUNCNAME}"; }' )"
+
+function ___tmp ()
+{
+
+    declare vars=(
+        comp_ents
+        I J
+    )
+
+    comp_ents=(
+        ""                                      -A  command
+        "$( compgen -A function function )"     -A  function
+        brewI                                   -F  _brew_install
+        brewU                                   -F  _brew_uninstall
+        brewu                                   -F  _brew_update
+        brewup                                  -F  _brew_upgrade
+        brews                                   -F  _brew_search
+        brewl                                   -F  _brew_list
+        "brewi brewh"                           -F  _brew_info
+        "$( compgen -c ssh.sso )"               -F  _ssh
+        keystone                                -W  "$( keystone bash-completion )"
+    )
+
+    for (( I=0; I<${#comp_ents[@]}; I+=3 ))
+    do
+        for J in ${comp_ents[${I}]}
+        do
+            [ -n "${J}" ] || continue
+            complete ${comp_ents[$((I+1))]} "${comp_ents[$((I+2))]}" ${J}
+        done
+    done
+
+}
+___tmp 1>&2
+unset -f ___tmp
