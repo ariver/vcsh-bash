af0c89ef2f0bf8337e07005eb5816db24c39cb57
Aaron River <aaron.river@rackspace.com>
2014-09-14 12:34:16 -0500

    load functions from other src dirs.

---
+++ .config/bash/rc.d/71_function_sourced.bash
@@ -0,0 +1,9 @@
+#! /dev/null/bash
+
+___bash_config_load "${HOME_SRC_GITHUB_WORK_MY_DIR}/bash_functions.git"/*.bash
+
+___bash_config_load "${HOME_SRC_GITHUB_DIR}/ariver/bash_functions.git"/*.bash
+
+. "${HOME_SRC_GITHUB_DIR}/runsthruit/bash_function_init.git/bash_function_init.bash_function"
+
+. "${HOME_SRC_GITHUB_DIR}/runsthruit/json2path.git/json2path.bash_function"
