1223b7c55f972b175126698ce03a720495de285a
Aaron River <aaron.river@rackspace.com>
2014-09-24 17:36:10 -0500

    i need my curl_env.. ugh.. also needs to be rewritten at some point.

--- .config/bash/rc.d/71_function_sourced.bash
+++ .config/bash/rc.d/71_function_sourced.bash
@@ -7,3 +7,5 @@ ___bash_config_load "${HOME_SRC_GITHUB_DIR}/ariver/bash_functions.git"/*.bash
 . "${HOME_SRC_GITHUB_DIR}/runsthruit/bash_function_init.git/bash_function_init.bash_function"
 
 . "${HOME_SRC_GITHUB_DIR}/runsthruit/json2path.git/json2path.bash_function"
+
+. "${HOME_SRC_GITHUB_DIR}/runsthruit/curl_env.git/curl_env.bash_function"
