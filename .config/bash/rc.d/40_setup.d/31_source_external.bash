8af0421511293a3b157aaaa55d1cccce0dfbf2aa
Aaron River <aaron.river@rackspace.com>
2014-10-18 06:58:21 -0500

    Use less specific HOME_SRC_DIR.

---
+++ .config/bash/rc.d/40_setup.d/31_source_external.bash
@@ -0,0 +1,11 @@
+#! /dev/null/bash
+
+___bash_config_load "${HOME_SRC_DIR}/github.rackspace.com/aaron-river/bash_functions.git"/*.bash
+
+___bash_config_load "${HOME_SRC_DIR}/github.com/ariver/bash_functions.git"/*.bash
+
+. "${HOME_SRC_DIR}/github.com/runsthruit/bash_function_init.git/bash_function_init.bash_function"
+
+. "${HOME_SRC_DIR}/github.com/runsthruit/json2path.git/json2path.bash_function"
+
+. "${HOME_SRC_DIR}/github.com/runsthruit/curl_env.git/curl_env.bash_function"
