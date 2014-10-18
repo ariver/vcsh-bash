b904047fbcb9ccffa4585769947c2b0fa3c3dc04
Aaron River <aaron.river@rackspace.com>
2014-10-18 06:59:09 -0500

    Trying out placing non-system paths first.

--- .config/bash/rc.d/30_export.d/05_paths_additional.bash
+++ .config/bash/rc.d/30_export.d/05_paths_additional.bash
@@ -3,15 +3,15 @@
 # Prefix alternatives path, append my own paths.
 bash_path_update \
         ~/.opt/alternators/{,s}bin \
-        PATH \
         ~/.local/{,s}bin \
         /usr/local/{,s}bin \
         /opt/local_macports/{,s}bin \
+        PATH \
 
 # Prefix alternatives manpath, append my own manpaths.
 bash_path_update \
         ~/.opt/alternators/man \
-        MANPATH \
         ~/.local{,/share}/man \
         /usr/local/{,share/}man \
-        /opt/local_macports/{,share/}man
+        /opt/local_macports/{,share/}man \
+        MANPATH \
