c3af5682450f91dbd2752f6b1913510071f5f2e6
A. River <ariver@runsthruit.com>
2014-11-15 06:11:17 -0600

    Prefix python user dir bin; Contain all in function.

--- .config/bash/rc.d/30_export.d/05_paths_additional.bash
+++ .config/bash/rc.d/30_export.d/05_paths_additional.bash
@@ -1,17 +1,27 @@
 #! /dev/null/bash
 
-# Prefix alternatives path, append my own paths.
-bash_path_update \
-        ~/.opt/alternators/{,s}bin \
-        ~/.local/{,s}bin \
-        /usr/local/{,s}bin \
-        /opt/local_macports/{,s}bin \
-        PATH \
+function ___tmp ()
+{
 
-# Prefix alternatives manpath, append my own manpaths.
-bash_path_update \
-        ~/.opt/alternators/man \
-        ~/.local{,/share}/man \
-        /usr/local/{,share/}man \
-        /opt/local_macports/{,share/}man \
-        MANPATH \
+    # Add default python user base bin dir to path.
+    PATH="$( python -c 'from site import getuserbase; print(getuserbase())' )/bin:${PATH}"
+
+    # Prefix alternatives path, append my own paths.
+    bash_path_update \
+            ~/.opt/alternators/{,s}bin \
+            PATH \
+            ~/.local/{,s}bin \
+            /usr/local/{,s}bin \
+            /opt/local_macports/{,s}bin \
+
+    # Prefix alternatives manpath, append my own manpaths.
+    bash_path_update \
+            ~/.opt/alternators/man \
+            MANPATH \
+            ~/.local{,/share}/man \
+            /usr/local/{,share/}man \
+            /opt/local_macports/{,share/}man \
+
+}
+___tmp "${@}" 1>&2
+unset -f ___tmp
