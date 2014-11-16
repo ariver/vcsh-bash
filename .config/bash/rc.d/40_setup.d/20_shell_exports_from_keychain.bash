dcc817495b4d96d5ae3450f1223f9edebdcc2b6d
Aaron River <aaron.river@rackspace.com>
2014-11-16 10:00:56 -0600

    Not using some of these env vars anymore.

--- .config/bash/rc.d/40_setup.d/20_shell_exports_from_keychain.bash
+++ .config/bash/rc.d/40_setup.d/20_shell_exports_from_keychain.bash
@@ -5,16 +5,9 @@ ___tmp ()
 {
 
     declare keychain_exports=(
-        GIT_HUB_API_TOKEN
         HOMEBREW_GITHUB_API_TOKEN
         EMAIL
         MAILDIR
-        SSH_ID_RSA_LOAD
-#        HOME_SRC_DIR
-#        HOME_SRC_GITHUB_DIR
-#        HOME_SRC_GITHUB_MY_DIR
-#        HOME_SRC_GITHUB_WORK_DIR
-#        HOME_SRC_GITHUB_WORK_MY_DIR
     )
     export ${keychain_exports[*]}
 
