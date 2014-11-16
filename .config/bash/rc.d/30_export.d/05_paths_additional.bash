9060c2292e9051102f2251b1c6b8004089bd2099
Aaron River <aaron.river@rackspace.com>
2014-11-16 09:59:13 -0600

    Put local bin back in front of default; Disable user dir for python. Going back to using homebrew version of python. Just feels cleaner to me to have virtualenv loaded next to python I'm using. It's nice to know, though, that I can use system python, and add libraries easily with pip to the user dir.

--- .config/bash/rc.d/30_export.d/05_paths_additional.bash
+++ .config/bash/rc.d/30_export.d/05_paths_additional.bash
@@ -4,23 +4,23 @@ function ___tmp ()
 {
 
     # Add default python user base bin dir to path.
-    PATH="$( python -c 'from site import getuserbase; print(getuserbase())' )/bin:${PATH}"
+    #PATH="$( python -c 'from site import getuserbase; print(getuserbase())' )/bin:${PATH}"
 
     # Prefix alternatives path, append my own paths.
     bash_path_update \
             ~/.opt/alternators/{,s}bin \
-            PATH \
             ~/.local/{,s}bin \
             /usr/local/{,s}bin \
             /opt/local_macports/{,s}bin \
+            PATH \
 
     # Prefix alternatives manpath, append my own manpaths.
     bash_path_update \
             ~/.opt/alternators/man \
-            MANPATH \
             ~/.local{,/share}/man \
             /usr/local/{,share/}man \
             /opt/local_macports/{,share/}man \
+            MANPATH \
 
 }
 ___tmp "${@}" 1>&2
